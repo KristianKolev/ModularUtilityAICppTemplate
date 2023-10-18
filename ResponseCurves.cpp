a1cf8cfb5b2c71e7183453e57f7d2b1f6bd4d577
KristianKolev <kristian.kolev@protonmail.com>
2023-10-18 22:04:53 +0200

    Added Linear and logistic graph classes.

--- ResponseCurves.cpp
+++ ResponseCurves.cpp
@@ -6,44 +6,78 @@
 class ResponseCurve 
 {
     public:
-    float Mslope {};
-    float Kexponent {};
-    float Xshift {};
-    float Yshift {};
-    float input {};
-    float score {};
+    double input {};
+    double Mslope {};
+    double Kexponent {};
+    double Xshift {};
+    double Yshift {};
+    double score {};
 
-    ResponseCurve ();
-    ResponseCurve (float I, float M, float K, float X, float Y) : input (I), Mslope (M), Kexponent(K), Xshift(X), Yshift(Y) {}
+    ResponseCurve () {}
+    ResponseCurve (double I, double M, double K, double X, double Y) : input (I), Mslope (M), Kexponent(K), Xshift(X), Yshift(Y) {}
+   
+    virtual ~ResponseCurve() {}
 
-    virtual int CalculateCurve() = 0;
-    float PrintGraph (int dataPoints) 
+    virtual double CalculateCurve() = 0;
+    void PrintGraph (int dataPoints) 
     {
-        std::cout << "Input", '/t', "Score";
-        for (i = 0, i < dataPoints, i++)
+        std::cout << "Input" << '\t' << "Score" << '\n';
+        for (double i = 0; i < dataPoints; i++)
         {
-            std::cout << input, '/t', CalculateCurve();
-            input =  1/dataPoints;
+            std::cout << input << '\t' << CalculateCurve() << '\n';
+            input =  i/dataPoints;
 
         }
-    }
-        
-}
+    }    
+};
 
 // Formula for linear/polynomial graph.
-class PolyCurve: public ResponseCurve {
+class PolyCurve: public ResponseCurve 
+{
     public:
-    int CalculateCurve() 
+    using ResponseCurve::ResponseCurve;
+    double CalculateCurve() 
+    {
+       
+        score = Mslope * pow((input - Xshift), Kexponent) + Yshift;
+        score = std::clamp(score, 0.0, 1.0);
+        return score;
+    }
+};
+
+// Formula for logistic graph. A logistic curve is a common S-shaped curve (sigmoid curve).
+// It might be worth it to look into the derivatives of the logistic function.
+class LogisticCurve: public ResponseCurve 
+{
+    public:
+    using ResponseCurve::ResponseCurve;
+    double CalculateCurve() 
+    {
+       
+        score = 
+        // Mslope * pow((input - Xshift), Kexponent) + Yshift;
+        score = std::clamp(score, 0.0, 1.0);
+        return score;
+    }
+};
+
+// Formula for logit graph. Mathematically, the logit is the inverse of the standard logistic function
+class LogitCurve: public ResponseCurve 
+{
+    public:
+    using ResponseCurve::ResponseCurve;
+    double CalculateCurve() 
     {
         //examine that 5 + 0.5 term
         score = ((Mslope * ( log10 ((input - Xshift)/(1 - input - Xshift)) )) / (5 + 0.5 + Yshift));
-        score = std::clamp(score, 0, 1);
+        score = std::clamp(score, 0.0, 1.0);
         return score;
     }
-}
+};
 
 int main ()
 {
-    
+    PolyCurve TestCurve(-100.0, -1.0, 1.0, 0.0, 1.15);
+    TestCurve.PrintGraph(10);
     return 0;
 }
