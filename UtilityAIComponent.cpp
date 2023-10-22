abc32e306ff3ad7853aa1c4369bd8b34bbcc3122
KristianKolev <kristian.kolev@protonmail.com>
2023-10-22 22:20:02 +0200

    Started work on UtilityAIComponent.

---
+++ UtilityAIComponent.cpp
@@ -0,0 +1,4 @@
+#include "ResponseCurves.h"
+#include "BehavioursAndActions.h"
+
+ActiveBehaviour
---
+++ UtilityAIComponent.h
@@ -0,0 +1,5 @@
+#include <map>
+#include "ResponseCurves.h"
+#include "BehavioursAndAction.h"
+
+double ScoreActions (std::map<Considerations, double>, Behaviour);
