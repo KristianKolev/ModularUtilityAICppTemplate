5b9090295be0c880425709df4183e8108b220ce3
KristianKolev <kristian.kolev@protonmail.com>
2023-10-23 21:10:19 +0100

    Built Response curve presets.

--- ResponseCurves.cpp
+++ ResponseCurves.cpp
@@ -2,8 +2,17 @@
 #include <algorithm>
 #include <iostream>
 #include "ResponseCurves.h"
+//#include "Math/UnrealMathUtility.h"
 
 // Not exposed to the Editor
+
+/* // normalize and clamp input
+void ResponseCurve::ResponseCurve(double input, CurveProperties& curveProperty) {
+        input = FMath::GetMappedRangeValueClamped 
+        (FVector2D(curveProperty.lowerClamp, curveProperty.upperClamp), FVector2D(0.0f, 1.0f), input) 
+
+}
+*/
 
 // Transforms the value from the knowledge map into a score from 0 to 1
 void ResponseCurve::PrintGraph(int dataPoints ) {
@@ -16,11 +25,15 @@ void ResponseCurve::PrintGraph(int dataPoints ) {
             std::cout << input << '\t' << CalculateCurve() << '\n';
         }
 };
-
+/*
+void ResponseCurve::selectPreset(CurvePresetTypes* CurvePresetType) {
+       curveProperty = CurvePresets[*CurvePresetType];
+}
+*/
 // Formula for linear/polynomial graph.
 double PolyCurve::CalculateCurve() {
        
-        score = Mslope * pow((input - Xshift), Kexponent) + Yshift;
+        score = curveProperty.slope * pow((input - curveProperty.xShift), curveProperty.exponent) + curveProperty.yShift;
         score = std::clamp(score, 0.0, 1.0);
         return score;
 };
@@ -28,8 +41,8 @@ double PolyCurve::CalculateCurve() {
 // Formula for logistic graph. A logistic curve is a common S-shaped curve (sigmoid curve).
 // It might be worth it to look into the derivatives of the logistic function.
 double LogisticCurve::CalculateCurve() {
-       
-        score = Mslope / (1 + exp(-10 * Kexponent * (input - 0.5 - Xshift))) + Yshift;
+        
+        score = curveProperty.slope / (1 + exp(-10 * curveProperty.exponent * (input - 0.5 - curveProperty.xShift))) + curveProperty.yShift;
         score = std::clamp(score, 0.0, 1.0);
         return score;
 };
@@ -37,14 +50,25 @@ double LogisticCurve::CalculateCurve() {
 // Formula for logit graph. Mathematically, the logit is the inverse of the standard logistic function
 double LogitCurve::CalculateCurve() {
         //examine that 5 + 0.5 term
-        score = ((Mslope * ( log10 ((input - Xshift)/(1 - input - Xshift)) )) / (5 + 0.5 + Yshift));
+        score = ((curveProperty.slope * ( log10 ((input - curveProperty.xShift)/(1 - input - curveProperty.xShift)) )) / (5 + 0.5 + curveProperty.yShift));
         score = std::clamp(score, 0.0, 1.0);
         return score;
 };
+void selecPreset (CurvePresetTypes CurvePresetType, PolyCurve curve, const std::map<CurvePresetTypes, CurveProperties>& map) 
+{
+       curve.curveProperty = map[CurvePresetType];
+       return;
+}
 
 int main ()
 {
-    LogisticCurve TestCurve(100, -1.0, 0.7, -0.05, 1.08);
-    TestCurve.PrintGraph(10);
+   // LogisticCurve TestCurve(100, {-1.0, 0.7, -0.05, 1.08});
+    // TestCurve.PrintGraph(10);
+    PolyCurve newpoly;
+    CurvePresetTypes newtype = linearFall;
+        selecPreset(newtype, newpoly, CurvePresets);
+  
     return 0;
 }
+
+
